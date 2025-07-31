0d6d3ad42c7ebfa29ef9282766d735ddf9ff8817
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2025-07-31 12:18:51 -0400

    Update Vids.jsx

--- src/pages/Vids.jsx
+++ src/pages/Vids.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 imnport YouTubeRotatorVids from ./components/YouTubeRotatorLatestVids.jsx
-import YouTubeRotatorVids.css from ./components/css/YouTubeRotatorLatestVids.css
+import YouTubeRotatorVids.css from ./components/css/YouTubeRotatortVids.css
 
 export default function Vids() {
   return (
     <div>
       <h1> LATEST VIDEOS</h1>
+
+      <YouTubeRotatorVids />
 
       
 
