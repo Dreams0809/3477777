25e6d27229dc03e8598aff94a6a1966a16c224b5
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2025-07-31 14:50:38 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Vids from "./pages/Vids.jsx";
 import Vlogs from "./pages/Vlogs.jsx";
 import Signup from "./pages/Signup.jsx"; // ✅ Import Signin page
 import Login from "./pages/Login.jsx";
+import Pods from "./pages/Pods.jsx";
 
 
 
