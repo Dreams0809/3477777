2c54e78a50dd07cd9e4521e868bf2f053851528f
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2025-07-31 15:01:37 -0400

    Update Pods.jsx

--- src/pages/Pods.jsx
+++ src/pages/Pods.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import '.components/css/SubscribeDropdown.css';
 
 export default function Pods() {
-  return <h1>Welcome to Vlogs</h1>;
-}
+  return(
+    <div>
+      <h1> 347 Podcast </h1>
+        <p> The 347podcast is a short form conversation with friends and guests that range from entertainers, organizers, and beyond. 
+          We like meeting no people, so no conversation is safe, and all guests are welcome. Come vibe with us.
+        </p>
+
+      <div className="subscribe-dropdown">
+        <button className="subscribe-btn">Subscribe</button>
+      <div className="dropdown-content">
+        <a href="https://www.youtube.com/@347pod" target="_blank" rel="noopener noreferrer">YouTube</a>
+        <a href="https://podcasts.apple.com/" target="_blank" rel="noopener noreferrer">Apple Podcasts</a>
+        <a href="https://open.spotify.com/" target="_blank" rel="noopener noreferrer">Spotify</a>
+      </div>
+    </div>
+};
